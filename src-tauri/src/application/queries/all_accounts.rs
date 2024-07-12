078cef14b99730b6575d0659394b91121a6a7204
Igor Trofimov <igor@litentry.com>
2024-07-12 16:02:57 +0200

    add queries

---
+++ src-tauri/src/application/queries/all_accounts.rs
@@ -0,0 +1,15 @@
+use crate::{
+    domain::account::Account, infrastructure::repositories::account_repository::AccountRepository,
+};
+
+// might consider some sort,filter, pagination later
+pub async fn all_accounts_query() -> Result<Vec<Account>, surrealdb::Error> {
+    let repository = AccountRepository::new();
+    // let mut accounts = vec![];
+    // if let Ok(result) = repository.get_all().await {
+    //     accounts = result;
+    // }
+
+    // Ok(accounts)
+    repository.get_all().await
+}
